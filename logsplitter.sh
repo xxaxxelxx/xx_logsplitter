c5df8ac45e80d92d9fe2c6a03013aaf195fc4ce0
xxaxxelxx <x@axxel.net>
2015-11-25 15:28:59 +0100

    updated

--- logsplitter.sh
+++ logsplitter.sh
@@ -2,6 +2,9 @@
 DEPOTDIR="/depot"
 SPLITBASEDIR="/customer"
 if [ "x$DEPOTDIR" == "x" ]; then exit; fi
+
+MAXAGE=366
+
 SLEEP=60
 
 test -d $SPLITBASEDIR || mkdir -p $SPLITBASEDIR
@@ -10,12 +13,16 @@ for CUSTOMER in $@; do
     test -d $SPLITBASEDIR/$CUSTOMER || mkdir -p $SPLITBASEDIR/$CUSTOMER
 done
 
+for CUSTOMER in $@; do
+    find $SPLITBASEDIR/$CUSTOMER -type f -mtime +${MAXAGE} -exec rm {} \;
+done
+
 while true; do
     for RAWFILE in $DEPOTDIR/*.unprocessed; do
 	test -r "$RAWFILE" || continue
 	RAWFILEBASE=$(basename "$RAWFILE")
 	for CUSTOMER in $@; do
-	    zless $RAWFILE | grep -v '127.0.0.1' | grep -v ' 0$' | grep $CUSTOMER | sed 's|intro.||' | gzip >> $SPLITBASEDIR/$CUSTOMER/access.$(date "+%Y-%m-%d").log.gz
+	    zless $RAWFILE | grep -v '127.0.0.1' | grep -v ' 0$'| grep -v 'listclients' | grep $CUSTOMER | sed 's|intro.||' | gzip >> $SPLITBASEDIR/$CUSTOMER/access.$(date "+%Y-%m-%d").log.gz
 	done
 	mv -f $RAWFILE ${RAWFILE%*\.unprocessed}
     done
