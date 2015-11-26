26f3f655a097c50d80d58f61ad082e9b822fc7ca
xxaxxelxx <x@axxel.net>
2015-11-26 10:32:06 +0100

    updated

--- logsplitter.sh
+++ logsplitter.sh
@@ -10,11 +10,11 @@ SLEEP=60
 test -d $SPLITBASEDIR || mkdir -p $SPLITBASEDIR
 
 for CUSTOMER in $@; do
-    test -d $SPLITBASEDIR/$CUSTOMER || mkdir -p $SPLITBASEDIR/$CUSTOMER
+    test -d $SPLITBASEDIR/$CUSTOMER/logs || mkdir -p $SPLITBASEDIR/$CUSTOMER/logs
 done
 
 for CUSTOMER in $@; do
-    find $SPLITBASEDIR/$CUSTOMER -type f -mtime +${MAXAGE} -exec rm {} \;
+    find $SPLITBASEDIR/$CUSTOMER/logs -type f -mtime +${MAXAGE} -exec rm {} \;
 done
 
 while true; do
@@ -22,7 +22,7 @@ while true; do
 	test -r "$RAWFILE" || continue
 	RAWFILEBASE=$(basename "$RAWFILE")
 	for CUSTOMER in $@; do
-	    zless $RAWFILE | grep -v '127.0.0.1' | grep -v ' 0$'| grep -v 'listclients' | grep $CUSTOMER | sed 's|intro.||' | gzip >> $SPLITBASEDIR/$CUSTOMER/access.$(date "+%Y-%m-%d").log.gz
+	    zless $RAWFILE | grep -v '127.0.0.1' | grep -v ' 0$'| grep -v 'listclients' | grep $CUSTOMER | sed 's|intro.||' | gzip >> $SPLITBASEDIR/$CUSTOMER/logs/access.$(date "+%Y-%m-%d").log.gz
 	done
 	mv -f $RAWFILE ${RAWFILE%*\.unprocessed}
     done
